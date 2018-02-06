304da4d9a2afeb40ba1d8beb137d2f47956c9ae4
jjones370 <johnjonesjr370@gmail.com>
2018-02-06 15:37:39 -0600

    changes that outline the script

--- src/game.java
+++ src/game.java
@@ -1,4 +1,21 @@
 
 public class game {
 //this class is the main and is responsible for making the game run
+	public static void main(String[]args){
+		//runs the game
+		
+	}
+	public void dealattack(){
+		//when attacking an enemy
+	}
+	public void takeattack(){
+		//when attacked by opponent
+	}
+	public void flee(){
+		//when you wanna be a little weenie
+	}
+	public void battle(){
+		//uses jframe to display the battle
+	}
+
 }
--- src/hero.java
+++ src/hero.java
@@ -1,4 +1,35 @@
 
 public class hero {
 //this class contains all the attributes for our hero
+	
+private int atk;
+private int hp;
+private int spd;
+private int def;
+private int spatk;
+private int spdef;
+private int exp;
+
+ public hero()
+ {
+	 atk = 10;
+	 def = 10;
+	 spd = 10;
+	 spatk = 10;
+	 spdef = 10;
+	 hp = 20;
+	 exp = 0;
+ }
+ 
+ public void levelUP(int[]a)
+ {
+	 exp = 0;
+	 atk += a[0];
+	 def += a[1];
+	 spd += a[2];
+	 spatk += a[3];
+	 spdef += a[4];
+	 hp += a[5];
+ }
+ 
 }
