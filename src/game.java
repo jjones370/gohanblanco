b11c6560462783790b966f200b3571f62ea6c9b4
jjones370 <johnjonesjr370@gmail.com>
2018-02-08 15:18:59 -0600

    this is uploaded

--- src/game.java
+++ src/game.java
@@ -2,16 +2,21 @@ import javax.swing.*;
 public class game extends JFrame {
 //this class is the main and is responsible for making the game run
 	JPanel game1 = new JPanel();
-	
+	JButton attack = new JButton( "" );
+	JButton flee = new JButton( "" );
+	JButton skills = new JButton( "" );
 	public game()
 	{
 		super ( "Swing Window" );
 		setSize(1000,1000);
 		setDefaultCloseOperation( EXIT_ON_CLOSE );
 		add( game1 );
-		setVisible( false );
-		
+		game1.add(attack);
+		game1.add(flee);
+		game1.add(skills);
+		setVisible( true );
 	}
+	
 	public static void main(String[]args){
 		//runs the game
 		game gui = new game();
