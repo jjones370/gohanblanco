07beb5048b7183b9b77c921e6a24bac0f7359f60
jjones370 <johnjonesjr370@gmail.com>
2018-02-14 15:19:22 -0600

    deleted andies usless junk and got flee to work
    alsjdflaksmdfa

--- cell_dorado.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-public class cell_dorado extends mob
-   {
-   cell_dorado()
-      {
-      image = new ImageIcon("cell dorado.png");
-      }
-   }
--- src/game.java
+++ src/game.java
@@ -1,34 +1,53 @@
 import java.awt.*;
 import javax.swing.*;
-public class game extends JFrame {
+import java.awt.event.*;
+import java.util.*;
+public class game extends JFrame implements ActionListener{
 //this class is the main and is responsible for making the game run
    JPanel game1 = new JPanel();
-   JButton attack = new JButton( "" );
-   JButton flee = new JButton( "" );
-   JButton skills = new JButton( "" );
+   JButton attack = new JButton( "Attack" );
+   JButton flee = new JButton( "Flee" );
+   //JButton skills = new JButton( "Skills" );
    Container contentPane = getContentPane();
    JPanel grid = new JPanel(new GridLayout(3,3));
-   public game(ImageIcon a,ImageIcon b)
+   hero player = new hero();
+   public game(hero h,ImageIcon b)
    {
       super ( "Swing Window" );
       setSize(1000,1000);
       setDefaultCloseOperation( EXIT_ON_CLOSE );
-      add( game1 );
-      JLabel meme2 = new JLabel("",a,JLabel.CENTER);
+     
       JLabel meme = new JLabel("",b,JLabel.CENTER);
       game1.add(attack);
       game1.add(flee);
-      game1.add(skills);
+      //game1.add(skills);
       game1.add(meme);
-  
+      attack.addActionListener( this );
+      //skills.addActionListener( this );
+      flee.addActionListener( this );
       
-      game1.add(meme2);
+    
       setVisible( true );
+      add( game1 );
    }
-	
+   public void  actionPerformed( ActionEvent event )
+   {
+	   
+	   if( event.getSource() == attack )
+	   {
+		   dealattack();
+	   }
+	   /*else if ( event.getSource() == skills);
+	   {
+		   
+	   }*/
+	   else if( event.getSource() == flee)
+	   {
+		   flee(player);
+	   }
+   }
    public static void main(String[]args){
    	//runs the game
-      hero player = new hero();
       cell_dorado cell_dorado = new cell_dorado();
       ImageIcon meme = new ImageIcon("Kamehameha.png");
       
@@ -40,8 +59,17 @@ public class game extends JFrame {
    public void takeattack(){
    	//when attacked by opponent
    }
-   public void flee(){
+   public boolean flee(hero h){
    	//when you wanna be a little weenie
+	   int sp = h.getSpeed();
+	   boolean escape = false;
+	   Random generator = new Random();
+	   int roll = generator.nextInt(20) + 1;
+	   if(sp > roll)
+	   {
+		  escape = true;
+	   }
+	   return escape;
    }
    public void battle(){
    	//uses jframe to display the battle
